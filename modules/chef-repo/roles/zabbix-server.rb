7db5a4f63ffeddd3d062293242ee629f67beb634
Suguru Akiho <suguru.akiho@gmail.com>
2014-10-23 04:21:07 +0000

    chef-repo move to other github repository

--- modules/chef-repo/roles/zabbix-server.rb
+++
@@ -1,8 +0,0 @@
-name "zabbix-server"
-description "zabbix for monitoring server"
-# env_run_lists "name" => ["recipe[name]"], "environment_name" => ["recipe[name::attribute]"]
-default_attributes()
-# override_attributes "node" => { "attribute" => [ "value", "value", "etc." ] }
-run_list([
-  "recipe[zabbix::server_install]"
-])
